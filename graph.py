948b0b6c4f9c6a04c6204d975c8e401cf4953fe6
Nick-Liu-0717 <glrt2793@gmail.com>
2024-11-26 22:03:41 +0800

    Adding cell

--- graph.py
+++ graph.py
@@ -21,7 +21,7 @@ class Window:
     def close(self):
         self.__running = False
         
-    def draw_line(self, Line, fill_color):
+    def draw_line(self, Line, fill_color="black"):
         Line.draw(self.__canvas, fill_color)
 
 class Point:
@@ -36,4 +36,37 @@ class Line:
     
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+        
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+        self.__win = win
     
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        self.top_left_corner = Point(self.__x1, self.__y1)
+        self.bottom_right_corner = Point(self.__x2, self.__y2)
+        self.top_right_corner = Point(self.__x2, self.__y1)
+        self.bottom_left_corner = Point(self.__x1, self.__y2)
+        if self.has_left_wall is True:
+            left_wall = Line(self.top_left_corner, self.bottom_left_corner)
+            self.__win.draw_line(left_wall)
+        if self.has_right_wall is True:
+            right_wall = Line(self.top_right_corner, self.bottom_right_corner)
+            self.__win.draw_line(right_wall)
+        if self.has_top_wall is True:
+            top_wall = Line(self.top_left_corner, self.top_right_corner)
+            self.__win.draw_line(top_wall)
+        if self.has_bottom_wall is True:
+            bottom_wall = Line(self.bottom_left_corner, self.bottom_right_corner)
+            self.__win.draw_line(bottom_wall)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from graph import Window, Point, Line
+from graph import *
 
 win = Window(800, 600)
-point1 = Point(555, 50)
-point2 = Point(100, 500)
-line = Line(point1, point2)
-win.draw_line(line, "red")
+# point1 = Point(555, 50)
+# point2 = Point(100, 500)
+# line = Line(point1, point2)
+cell = Cell(win)
+cell.draw(100, 300, 200, 600)
 win.wait_for_close()
